bd9bd224af7d0c6160b7545d592ea5ef5c3b7334
Duong Phuc Trang <trangbg2000@gmail.com>
2023-02-04 17:39:27 +0700

    done ver1

---
+++ src/pages/api/card.ts
@@ -0,0 +1,47 @@
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import fs from 'fs-extra'
+// import handler from './hello'
+// type Data = {
+//   name: string
+// }
+// export default function handler(req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     const data = await fs.readFile('path/to/file', 'utf8');
+//     res.status(200).json({ data });
+//   } catch (err) {
+//     res.status(500).json({ message: 'Error reading file', error: err });
+//   }
+// }
+
+
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import * as fs from 'fs';
+import path from "path"
+type Data = {
+  definition: string,
+  value: string,
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const data: Array<Data> = []
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), 'public');
+  //Read the json data file data.json
+  const text = fs.readFileSync(jsonDirectory + '/text.txt', 'utf-8')
+  const lines = text.split('\n')
+
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].includes('=')) {
+      const term = lines[i].split('=');
+      data.push({ definition: term[0], value: term[1] })
+    }
+  }
+
+  res.status(200).json(data)
+}
